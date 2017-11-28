a293b0a8f24cbfdaac05eb1fe402ce160130b0a5
Alok Hota <ahota@vols.utk.edu>
2017-11-28 17:01:46 -0600

    implemented proper LSB with OPAP
    LSB does NOT work with YCbCr on the luma channel, because the conversion
    alone destroys the embedded data
    
    Seems to work with any of the RGB channels
    
    Also by default it saves to disk as a BMP image to avoid compression,
    which works

--- goldfish/watermarker.py
+++ goldfish/watermarker.py
@@ -1,6 +1,7 @@
 import numpy
 import random
 from PIL import Image
+import sys
 
 def seeded(func):
     def func_wrapper(self, *args, **kwargs):
@@ -13,10 +14,16 @@ class Watermarker(object):
     Base watermarking algorithm class. Implements LSB embedding and
     extracting and exposes the embed() and extract() API for child
     classes
+    LSB method uses OPAP from C. Chan and L. Cheng, Hiding data in images
+    by simple LSB substitution, 2004.
     '''
-    def __init__(self, debug=False):
+    def __init__(self, bits_per_pixel=2, chan='R', debug=False):
         self.rng = random.Random()
         self.debug = debug
+        self.k_bits = bits_per_pixel
+        self.symbols = []
+        self.mode = 'RGB'
+        self.band = self.mode.index(chan)
 
     @seeded
     def embed(self, image, message):
@@ -24,22 +31,54 @@ class Watermarker(object):
             image = Image.open(image)
 
         width, height = image.size
+
+        yimage = image.convert(self.mode)
         if len(image.getbands()) > 3:
-            bands = self._get_bands(image)[:3]
+            bands = self._get_bands(yimage)[:3]
         else:
-            bands = self._get_bands(image)
+            bands = self._get_bands(yimage)
 
         bin_message = ''.join([format(ord(c), 'b').zfill(8) for c in message])
-        locations = zip(self.rng.sample(xrange(height), len(bin_message)),
-                        self.rng.sample(xrange(width ), len(bin_message)))
-        channels = [self.rng.choice(range(len(bands)))
-                    for i in range(len(bin_message))]
 
-        for b_i, bit in enumerate(bin_message):
-            original = format(bands[channels[b_i]][locations[b_i]], 'b')[:-1]
-            bands[channels[b_i]][locations[b_i]] = int(original+bit, 2)
+        n_places = len(bin_message)/self.k_bits
+        if len(bin_message) % self.k_bits != 0:
+            n_places += 1
 
-        return Image.merge('RGB', [Image.fromarray(band) for band in bands])
+        # locations to embed data in, randomly located across the image
+        locations = zip(self.rng.sample(xrange(height), n_places),
+                        self.rng.sample(xrange(width ), n_places))
+        luma = bands[self.band]
+
+        for b_i in range(n_places):
+            # given a symbol and a location, embed the symbol in the last
+            # k bits of the pixel value at the location
+            symbol = bin_message[b_i*self.k_bits:b_i*self.k_bits+self.k_bits]
+            original_val = luma[locations[b_i]]
+            original = format(original_val, 'b').zfill(8)
+            self.symbols.append([symbol])
+            stego = original[:-self.k_bits] + symbol
+            stego_val = int(stego, 2)
+            # apply OPAP on the stego pixel
+            # this is supposed to minimize the error introduced in embedding
+            # first find which interval the error exists within
+            delta = stego_val - original_val
+            optimized = stego_val # default for some cases
+            # three intervals
+            if 2**(self.k_bits-1) < delta < 2**self.k_bits:
+                if stego_val >= 2**self.k_bits:
+                    optimized = stego_val - 2**self.k_bits
+            elif -2**(self.k_bits-1) <= delta <= 2**(self.k_bits-1):
+                pass # just use the default option above
+            elif -2**(self.k_bits) < delta < -2**(self.k_bits-1):
+                if stego_val < 256 - 2**(self.k_bits):
+                    optimized = stego_val + 2**self.k_bits
+            luma[locations[b_i]] = optimized
+
+        bands[self.band] = luma
+
+        watermarked = Image.merge(self.mode,
+                [Image.fromarray(band) for band in bands])
+        return watermarked
 
     @seeded
     def extract(self, image, message_length=256):
@@ -47,19 +86,29 @@ class Watermarker(object):
             image = Image.open(image)
 
         width, height = image.size
-        if len(image.getbands()) > 3:
-            bands = self._get_bands(image)[:3]
-        else:
-            bands = self._get_bands(image)
 
-        locations = zip(self.rng.sample(xrange(height), message_length),
-                        self.rng.sample(xrange(width ), message_length))
-        channels = [self.rng.choice(range(len(bands)))
-                    for i in range(message_length)]
+        yimage = image.convert(self.mode)
+        bands = self._get_bands(yimage)
 
-        bin_message = ''.join(
-                [format(bands[channels[b_i]][locations[b_i]],'b')[-1]
-                 for b_i in range(message_length)])
+        n_places = message_length/self.k_bits
+        if message_length % self.k_bits != 0:
+            n_places += 1
+
+        locations = zip(self.rng.sample(xrange(height), n_places),
+                        self.rng.sample(xrange(width ), n_places))
+        luma = bands[self.band]
+
+        bin_message = ''
+        for b_i in range(n_places):
+            data = format(luma[locations[b_i]], 'b').zfill(8)
+            symbol = data[-self.k_bits:]
+            self.symbols[b_i].append(symbol)
+            bin_message += symbol
+
+        '''
+        for symbol in self.symbols:
+            print symbol[0], '->', symbol[1]
+        '''
 
         return ''.join([chr(int(bin_message[i:i+8], 2)) 
                  for i in range(0, len(bin_message), 8)])
--- test/LSBTest.py
+++ test/LSBTest.py
@@ -1,45 +1,94 @@
-'''
-from goldfish.Embedders import LSBEmbedder
-from goldfish.Extractors import LSBExtractor
-'''
 import sys
 sys.path.append('../')
 from goldfish.watermarker import Watermarker
 import uuid
 
-from PIL import Image, ImageMath
+from PIL import Image, ImageMath, ImageDraw, ImageFont
+from argparse import ArgumentParser
 
-if len(sys.argv) < 2:
-    print 'Usage:'
-    print '\t', sys.argv[0], 'image_filepath'
-    sys.exit(1)
+fnt = ImageFont.load_default().font
 
-infile = sys.argv[1]
-outfile = infile[:-4]+'-altered.jpg'
+channels = ('R', 'G', 'B')
+imagetypes = ('bmp', 'jpeg', 'png')
 
-message = uuid.uuid4().hex
-wm = Watermarker()
+parser = ArgumentParser()
+parser.add_argument('image', help='Path to the image to test with')
+parser.add_argument('-n', '--n-rounds', type=int, default=1,
+        help='Number of rounds to perform')
+parser.add_argument('-t', '--type', choices=imagetypes, default=imagetypes[0],
+        help='Filetype to save during test')
+parser.add_argument('-q', '--quality', type=int, default=75,
+        help='Compression quality watermark should resist')
+parser.add_argument('-k', '--bits-per-pixel', type=int, default=2,
+        help='Bits per pixel to embed')
+parser.add_argument('-c', '--channel', choices=channels, default=channels[0],
+        help='Channel to embed in')
+parser.add_argument('--debug', action='store_true',
+        help='Enable debug messages')
+parser.add_argument('--super-debug', action='store_true',
+        help='Enable debug messages and show embedding')
+parser.add_argument('--quiet', action='store_true',
+        help='Disable all output')
+parser.add_argument('-d', '--data-size', type=int, default=32,
+        help='How many bytes of watermark data to embed/extract')
+parser.add_argument('--direct', action='store_true',
+        help='Directly decode the watermarked image to test insertions/deletions')
+args = parser.parse_args()
 
-print 'Embedding message \"'+message+'\" into image'
+print_len = len(str(args.n_rounds))
+if args.super_debug:
+    args.debug = True
+successes = 0
 
-im_out = wm.embed(infile, message)
+for i in range(args.n_rounds):
+    if not args.quiet:
+        print '{num:{w}}/{t}'.format(num=i+1, w=print_len, t=args.n_rounds)
+    infile = sys.argv[1]
+    outfile = infile[:-4]+'-altered.' + args.type
 
-# only reading back from image in memory
-# LSB doesn't survive most image compression
-retrieved = wm.extract(im_out)
+    message = uuid.uuid4().hex
+    wm = Watermarker(bits_per_pixel=args.bits_per_pixel,
+                     chan=args.channel,
+                     debug=args.debug)
 
-if message != retrieved:
-    print 'Failure!'
-    print message
-    print retrieved
-else:
-    print 'Success!'
+    if args.debug and not args.quiet:
+        print 'Embedding message \"'+message+'\" into image'
 
+    im = Image.open(infile)
+    im_out = wm.embed(infile, message)
+
+    if args.super_debug:
+        im.show()
+        im_out.show()
+        sys.exit()
+
+    # only reading back from image in memory
+    # LSB doesn't survive most image compression
+    if args.debug and not args.quiet:
+        print 'Extracting'
+    if args.direct:
+        retrieved = wm.extract(im_out)
+    else:
+        im_out.save(outfile)
+        retrieved = wm.extract(outfile)
+
+    if message != retrieved:
+        if args.debug and not args.quiet:
+            print 'Failure!'
+            print message
+            print retrieved
+    else:
+        if args.debug and not args.quiet:
+            print 'Success!'
+        successes += 1
+
+print successes, 'out of', args.n_rounds, 'successful'
+
+'''
 print 'Getting the diff'
 im_in = Image.open(infile)
 in_bands = im_in.split()
 out_bands = im_out.split()
-diffs = [ImageMath.eval("convert(a - b, 'L')", a=in_bands[i], b=out_bands[i])
-        for i in range(len(in_bands))]
-diff = Image.merge('RGB', diffs)
+diff = ImageMath.eval("convert(b - a, 'L')", a=in_bands[0], b=out_bands[0])
 diff.save('diff.png')
+'''
